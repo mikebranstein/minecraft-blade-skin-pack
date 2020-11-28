fe286462bf8858fab6c423d8c5cda9bd30c44d99
Mike Branstein <mikeb@kizan.com>
2020-11-28 09:59:26 -0500

    import mcreator workspace

--- build.bat
+++
@@ -1,6 +0,0 @@
-set PACK_BASE=%localappdata%\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\development_resource_packs\blade_pack
-
-mkdir %PACK_BASE%
-
-copy *.* %PACK_BASE%
-
